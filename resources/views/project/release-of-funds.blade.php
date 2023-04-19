f5e4e33041de9f54560420dd826d30924d74e004
voseji <vooseji@emgeresources.com>
2023-04-19 12:35:07 +0100

    authentication sorted

--- resources/views/project/release-of-funds.blade.php
+++ resources/views/project/release-of-funds.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<title>LAVEMS Site Inspection Form</title>
+<title>LAVEMS Release of Funds Form</title>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>file_1681147039727</title>
